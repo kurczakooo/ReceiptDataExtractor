e78b693c1e48d945b71762899d53bc6dd61334b7
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2024-09-22 23:07:57 +0200

    next step, get reciepts scans and optimize tesseract

--- main.py
+++ main.py
@@ -8,15 +8,41 @@ example_path = 'data//' + photos_list[0]
 
 image = cv2.imread(example_path)
 
+#-----------------------------------------------------------------------------------------------
+# Scaling the image
+scale_percent = 150  # Percentage of original size
+width = int(image.shape[1] * scale_percent / 100)
+height = int(image.shape[0] * scale_percent / 100)
+dim = (width, height)
+
+# Resize image
+resized_image = cv2.resize(image, dim, interpolation=cv2.INTER_LINEAR)
+
 # Convert to grayscale
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
+
+# Zwiększenie kontrastu za pomocą CLAHE
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+contrast_enhanced = clahe.apply(gray)
 
 # Gaussian blur
-blur = cv2.GaussianBlur(gray, (5, 5), 0)
+blur = cv2.GaussianBlur(contrast_enhanced, (5, 5), 0)
+
+#------------------------------------------------------------------------------------------------
+
 
 # Adaptive thresholding
 thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-# Use Tesseract to extract text
-text = pytesseract.image_to_string(thresh)
+
+# Użycie Tesseract OCR do rozpoznania tekstu
+custom_config = r'--oem 3 --psm 6'  # Opcje konfiguracyjne Tesseract
+text = pytesseract.image_to_string(thresh, config=custom_config, lang='pol')
 
 print(text)
+
+
+
+"""# Pokaż wynik
+cv2.imshow('post optimizing', blur)
+cv2.waitKey(0)
+cv2.destroyAllWindows()"""
