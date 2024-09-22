242dbfeaae436f9c6768c36e4b9b0a1b15644233
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2024-09-22 16:38:04 +0200

    got tesseract working

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import cv2
 import pytesseract
 import os
 
-pytesseract.pytesseract.tesseract_cmd = r'<full_path_to_your_tesseract_executable>'
-
-
 photos_list = os.listdir('data//')
 
 example_path = 'data//' + photos_list[0]
@@ -14,14 +11,12 @@ image = cv2.imread(example_path)
 # Convert to grayscale
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Apply thresholding
-_, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY_INV)
+# Gaussian blur
+blur = cv2.GaussianBlur(gray, (5, 5), 0)
 
+# Adaptive thresholding
+thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 # Use Tesseract to extract text
 text = pytesseract.image_to_string(thresh)
 
 print(text)
-
-
-
-https://github.com/h/pytesseract
