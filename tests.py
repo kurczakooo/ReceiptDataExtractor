eabe485012ce98afdb07f4d738ac2fa03181bdd5
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2024-09-23 21:09:36 +0200

    on my way to compare the results

--- tests.py
+++ tests.py
@@ -3,3 +3,12 @@ import os
 import pandas as pd
 
 
+list = os.listdir('results//')
+
+for i in range(len(list)):
+    list[i] = os.path.join('results//', list[i])
+
+validation = list[-1]
+tests = list[:-1]
+
+
